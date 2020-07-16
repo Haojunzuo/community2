b51a123eb095fdcfe6241f8ab28b8e115ace6a26
Haojunzuo <2428393291@qq.com>
2020-07-16 09:15:12 +0800

    富文本编辑

---
+++ src/main/java/com/cjh/community2/controller/FileController.java
@@ -0,0 +1,18 @@
+package com.cjh.community2.controller;
+
+import com.cjh.community2.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @ResponseBody
+    @RequestMapping("/file/upload")
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/cjh.jpg");
+        return fileDTO;
+    }
+}
---
+++ src/main/java/com/cjh/community2/dto/FileDTO.java
@@ -0,0 +1,10 @@
+package com.cjh.community2.dto;
+
+import lombok.Data;
+
+@Data
+public class FileDTO {
+    private int success;
+    private String message;
+    private String url;
+}
